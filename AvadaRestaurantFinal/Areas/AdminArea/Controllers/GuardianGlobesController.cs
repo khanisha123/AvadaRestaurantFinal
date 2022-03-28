e7fb6daee67197b44f0a2d28ae825d1cf8b015b3
khanisha123 <khanisha@code.edu.az>
2022-03-28 21:45:02 +0400

    GuardianGlobes crud is ready

--- AvadaRestaurantFinal/Areas/AdminArea/Controllers/GuardianGlobesController.cs
+++ AvadaRestaurantFinal/Areas/AdminArea/Controllers/GuardianGlobesController.cs
@@ -1,8 +1,11 @@
 ﻿using AvadaRestaurantFinal.DAL;
+using AvadaRestaurantFinal.Extension;
 using AvadaRestaurantFinal.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,14 +15,107 @@ namespace AvadaRestaurantFinal.Areas.AdminArea.Controllers
     public class GuardianGlobesController : Controller
     {
         private readonly Context _context;
-        public GuardianGlobesController(Context context)
+        private readonly IWebHostEnvironment _env;
+        public GuardianGlobesController(Context context, IWebHostEnvironment env)
         {
             _context = context;
+            _env = env;
         }
         public IActionResult Index()
         {
             List<GuardianGlobe> guardianGlobes = _context.GuardianGlobe.ToList();
             return View(guardianGlobes);
+        }
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(GuardianGlobe guardianGlobe)
+        {
+            if (ModelState["Photo"].ValidationState == Microsoft.AspNetCore.Mvc.ModelBinding.ModelValidationState.Invalid)
+            {
+                ModelState.AddModelError("Photo", "Do not empty");
+            }
+
+            if (!guardianGlobe.Photo.ContentType.Contains("image/"))
+            {
+                ModelState.AddModelError("Photo", "only image");
+                return View();
+            }
+            if (guardianGlobe.Photo.Length / 1024 > 300)
+            {
+                ModelState.AddModelError("Photo", "300den yuxari ola bilmez");
+                return View();
+            }
+
+            string FileName = Guid.NewGuid() + guardianGlobe.Photo.FileName;
+            string path = Path.Combine(_env.WebRootPath, "img", FileName);
+            FileStream fileStream = new FileStream(path, FileMode.Create);
+            await guardianGlobe.Photo.CopyToAsync(fileStream);
+            guardianGlobe.ImageUrl = FileName;
+
+            await _context.GuardianGlobe.AddAsync(guardianGlobe);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+        }
+        public IActionResult Delete(int id)
+        {
+            var findId = _context.GuardianGlobe.Find(id);
+            string path = Path.Combine(_env.WebRootPath, findId.ImageUrl);
+            if (System.IO.File.Exists(path))
+            {
+                System.IO.File.Delete(path);
+            }
+            _context.GuardianGlobe.Remove(findId);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null) return NotFound();
+            GuardianGlobe dbGuardianGlobe = await _context.GuardianGlobe.FindAsync(id);
+            if (dbGuardianGlobe == null) return NotFound();
+            return View(dbGuardianGlobe);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int? id, GuardianGlobe guardianGlobe)
+        {
+            if (id == null) return NotFound();
+            if (ModelState["Photo"].ValidationState == Microsoft.AspNetCore.Mvc.ModelBinding.ModelValidationState.Invalid)
+            {
+                ModelState.AddModelError("Photo", "Do not empty");
+            }
+
+            if (!guardianGlobe.Photo.ContentType.Contains("image/"))
+            {
+                ModelState.AddModelError("Photo", "only image");
+                return View();
+            }
+            if (guardianGlobe.Photo.Length / 1024 > 300)
+            {
+                ModelState.AddModelError("Photo", "300den yuxari ola bilmez");
+                return View();
+            }
+            GuardianGlobe dbGuardianGlobe = await _context.GuardianGlobe.FindAsync(id);
+            string path = Path.Combine(_env.WebRootPath, dbGuardianGlobe.ImageUrl);
+            if (System.IO.File.Exists(path))
+            {
+                System.IO.File.Delete(path);
+            }
+            string fileName = await guardianGlobe.Photo.SaveImageAsync(_env.WebRootPath, "img");
+            dbGuardianGlobe.ImageUrl = fileName;
+            dbGuardianGlobe.Name = guardianGlobe.Name;
+            dbGuardianGlobe.profession = guardianGlobe.profession;
+            dbGuardianGlobe.Description = guardianGlobe.Description;
+            dbGuardianGlobe.ButtonName = guardianGlobe.ButtonName;
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
